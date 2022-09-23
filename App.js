87126d0b82ccfad96d2c121e00a63a27bfd0c2ee
Rolan Veron Cruz <rolan@upshot.global>
2022-09-23 20:50:49 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App: () => Node = () => {
+
+  return (
+    <Text> Hello World! </Text>
+  );
+};
+
+
+export default App;
