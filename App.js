c8d7af5f6bb939b2af566c43cdd6818c65556a0f
Rolan Veron Cruz <rolan@upshot.global>
2022-09-26 10:45:08 +0800

    BottomSheet seems to work

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 
-import React from 'react';
-import type {Node} from 'react';
+import React, {useCallback, useMemo, useRef } from 'react';
 import {
+  StyleSheet,
   Text,
   View,
+  Button,
 } from 'react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
 
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
@@ -12,10 +14,43 @@ import {
 
 const App: () => Node = () => {
 
+ const bottomSheetRef = useRef(null);
+
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  },[]);
+
   return (
-    <Text> Hello World! </Text>
+      <View style={styles.container}>
+        <BottomSheet
+          ref={bottomSheetRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChanges}
+        >
+        <View style={styles.contentContainer}>
+           <Text>Awesome 🎉</Text>
+           <Button title="Hello, World">Hello</Button>
+        </View>
+       </BottomSheet>
+      </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: 'grey',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
 
 export default App;
